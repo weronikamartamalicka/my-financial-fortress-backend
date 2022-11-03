6c17a74dc4697c65c0e77b878d4c383a25001e86
Weronika Malicka <weronikamarta.malicka@gmail.com>
2022-11-03 10:22:10 +0100

    Corrected BondsQuotedFoundException- super(message)

--- financial-fortress-backend/src/main/java/com/restapi/financialfortressbackend/exception/BondsQuotedNotFoundException.java
+++ financial-fortress-backend/src/main/java/com/restapi/financialfortressbackend/exception/BondsQuotedNotFoundException.java
@@ -1,4 +1,8 @@
 package com.restapi.financialfortressbackend.exception;
 
 public class BondsQuotedNotFoundException extends Exception {
+
+    public BondsQuotedNotFoundException(String message) {
+        super(message);
+    }
 }
